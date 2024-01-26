b5821bd5a4254dd083950d8eece5be6753e8ae89
Abrar <108106145+abrarmurad416@users.noreply.github.com>
2024-01-26 07:20:02 +0000

    skill list changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -32,11 +32,11 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Full stack development",
+  "Database design",
+  "DevOps",
+  "Project Management",
+  "Cloud computing",
 ];
 
 /**
