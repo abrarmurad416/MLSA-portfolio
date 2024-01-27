666dddcce7ddbb29a6ade45d96fc81608862b4e0
Abrar <108106145+abrarmurad416@users.noreply.github.com>
2024-01-27 02:58:10 +0000

    Added education

---
+++ src/Components/Education.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Education = () => {
+    return(
+        <section className="light" id="education">
+            <h2>Education</h2>
+            <div>
+                <div>
+                    <h3>Wilfrid Laurier University</h3>
+                    <p>Bachelor of Computer Science</p>
+                    <p>2022 - 2026</p>
+                </div>
+                <br />
+                <div>
+                    <h3>North Park High School</h3>
+                    <p>High school diploma</p>
+                    <p>2018 - 2022</p>
+                </div>
+            </div>            
+        </section>
+    )
+}
+
+export default Education;
